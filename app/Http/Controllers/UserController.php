cdd712d38b3d89c2f4718cd6c6961524b4d19752
Alexander Podlesnyi <gwebdzen@gmail.com>
2019-07-27 22:38:24 +0300

    Add personal account

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,15 +3,58 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use App\User;
+use App\Helpers\Loger;
 class UserController extends Controller
 {
     public function index()
     {
+        $id = Auth::id();
 
+        $user = User::find($id);
+
+        return view("content/account",compact("user"));
     }
-    public function update()
+
+    /**
+     * Обновление пользователя и запись в лог
+     */
+    public function update(Request $request)
     {
 
+        $test = [
+            "hobby" => ["required","string","max:60"],
+            "email" => ["required",'string', 'email'],
+        ];
+        if(!empty($request->password)){
+            $test['password'] = ['string', 'min:4', 'confirmed'];
+        }
+
+        $test = $this->validate($request, $test);
+
+        // Если есть пароль нужно его захешить для сравнения
+        if(isset($test['password'])){
+            $test['password'] = bcrypt($test['password']);
+        }
+
+        $id = Auth::id();
+
+        // Запускаем транзакцию, что бы логи и записи точно добавились вместе.
+        DB::beginTransaction();
+
+
+
+        // Нам возможно нужен пароль, а модель его скрывает.
+        $user = DB::table('users')->where("id","=",$id)->first();
+
+        DB::table('users')
+            ->where('id', $id)
+            ->update($test);
+
+        Loger::add_commit("update_user",array("user"=>$id),$user, $test);
+
+        return redirect()->back()->with('message', 'Изменения добавлены');
     }
 }
---
+++ resources/views/content/account.blade.php
@@ -0,0 +1,101 @@
+@extends('index',[
+        'title' => "Аккаунт",
+        "prefix_body" => 'acount',
+    ])
+@section('content')
+    <h1 class="text-center">Аккаунт</h1>
+    <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-body">
+                            <form method="POST" action="{{ route('account') }}">
+                                @csrf
+                                <input type="hidden" name="log_action" value="update"  />
+                                <input type="hidden" name="id" value="{{ $user->id }}"  />
+                                <div class="form-group row">
+                                    <label class="col-md-4 col-form-label text-md-right">{{ __('ФИО') }}</label>
+                                    <div class="col-md-6 form_string">
+                                        {{ $user->full_name }}
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label class="col-md-4 col-form-label text-md-right">{{ __('Мобильный') }}</label>
+                                    <div class="col-md-6 form_string">
+                                        {{ $user->mob_phone }}
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="hobby" class="col-md-4 col-form-label text-md-right">{{ __('Хобби') }}</label>
+
+                                    <div class="col-md-6">
+                                        <input id="hobby" type="text" class="form-control @error('hobby') is-invalid @enderror" name="hobby" value="{{ old('hobby',$user->hobby) }}" required autocomplete="hobby" autofocus>
+
+                                        @error('hobby')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label class="col-md-4 col-form-label text-md-right">{{ __('День рождения') }}</label>
+
+                                    <div class="col-md-6 birthday form_string">
+                                        {{ $user->birthday }}
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Адрес') }}</label>
+
+                                    <div class="col-md-6">
+                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email',$user->email) }}" required autocomplete="email">
+
+                                        @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Сменить пароль') }}</label>
+
+                                    <div class="col-md-6">
+                                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="new-password">
+
+                                        @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Подтверждение пароля') }}</label>
+
+                                    <div class="col-md-6">
+                                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" autocomplete="new-password">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row mb-0">
+                                    <div class="col-md-6 offset-md-4">
+                                        <button type="submit" class="btn btn-primary">
+                                            {{ __('Register') }}
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+@endsection
